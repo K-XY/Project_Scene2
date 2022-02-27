a58952bec15af36baa049651b7bfcd94ba0a0fcc
K-XY <98725521+K-XY@users.noreply.github.com>
2022-02-27 22:43:13 +0800

    Add files via upload

---
+++ C_Testing.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+
+
+class TestUrlMethods(unittest.TestCase):
+   def is_url(self, url, ls):
+    self.assertEqual
+    ls(url,'http://192.168.17.84/freebix')
+
+if __name__ == '__newscrapy__':
+    unittest.main()
